2a5d2cb90fd0b99ab391e00a956475c99b3c6ab1
Michael Dougras da Silva <micdoug.silva@gmail.com>
2021-02-17 15:48:58 -0300

    Finished lesson 5.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import Menu from '../src/components/Menu';
+import Footer from '../src/commons/Footer';
+import styled from 'styled-components';
+
+const HomeWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 export default function Home() {
-  return <div>
+  return <HomeWrapper>
     <Menu />
-  </div>
+    <Footer></Footer>
+  </HomeWrapper>
 }
